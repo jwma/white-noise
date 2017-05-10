987bea3bcd94bf33501bee356cf07d82b7f9f1a6
jwma <mjw@majiawei.com>
2017-05-10 23:18:30 +0800

    1. 调整代码并加入适当的注释

--- white-noise/ViewController.swift
+++ white-noise/ViewController.swift
@@ -11,7 +11,9 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    /// 记录不同类型音频跟起对应的播放器
     var sounds: [String: NSObject] = [String: NSObject]()
+    /// 记录不同类型音频跟起对应 UI 控件的 Tag 值
     let mapping: [String: String] = ["100": "thunder", "200": "raining"]
     
     override func viewDidLoad() {
@@ -21,13 +23,19 @@ class ViewController: UIViewController {
         readyPlayer()
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /// 创建不同类型的音频播放器
     func readyPlayer() {
         
         createPlayerByType(type: "thunder")
         createPlayerByType(type: "raining")
         
+        /// 实现后台播放
         let audioSession = AVAudioSession.sharedInstance()
-        
         do {
             try audioSession.setCategory(AVAudioSessionCategoryPlayback)
         } catch {
@@ -35,9 +43,12 @@ class ViewController: UIViewController {
         }
     }
     
+    /// 根据音频类型创建播放器并将其关联到 sounds 字典中
     func createPlayerByType(type: String) {
         do {
+            
             let player = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: type, ofType: "mp3")!))
+            
             player.prepareToPlay()
             player.numberOfLoops = -1
             player.volume = 0.5
@@ -47,12 +58,8 @@ class ViewController: UIViewController {
             print(error)
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    /// 根据 UI 控件传递的 Tag 值来获取对应类型的播放器
     func getPlayerByTag(tag: Int) -> AVAudioPlayer{
         let tag = "\(tag)"
         let type = mapping[tag]
